c386553ab1eafbc43430dbba1cff179b1c402430
Richard Neumann <r.neumann@homeinfo.de>
2017-06-29 09:18:53 +0200

    Refactored methods

--- appcmd/wsgi.py
+++ appcmd/wsgi.py
@@ -32,19 +32,24 @@ class CommonBasicHandler(ResourceHandler):
         """Returns the presentation ID"""
         try:
             return int(self.query['tid'])
-        except KeyError:
-            raise self.logerr('No TID specified.') from None
-        except (TypeError, ValueError):
+        except (KeyError, TypeError):
+            return None
+        except ValueError:
             raise self.logerr('TID must be an integer.') from None
 
     @property
     def terminal(self):
         """Returns the respective customer"""
-        try:
-            return Terminal.by_ids(self.cid, self.tid)
-        except DoesNotExist:
-            raise self.logerr('No such terminal: {}.{}.'.format(
-                self.tid, self.cid)) from None
+        tid = self.tid
+
+        if tid is None:
+            raise self.logerr('No TID specified.') from None
+        else:
+            try:
+                return Terminal.by_ids(self.cid, self.tid)
+            except DoesNotExist:
+                raise self.logerr('No such terminal: {}.{}.'.format(
+                    self.tid, self.cid)) from None
 
 
 class PrivateHandler(CommonBasicHandler):
@@ -53,15 +58,15 @@ class PrivateHandler(CommonBasicHandler):
     def post(self):
         """Handles POST requests"""
         if self.resource == 'contactform':
-            return self._contact_mail()
+            return self.contact_mail()
         elif self.resource == 'tenant2tenant':
-            return self._tenant2tenant()
+            return self.tenant2tenant()
         elif self.resource == 'damagereport':
-            return self._damage_report()
+            return self.damage_report()
         else:
             raise self.logerr('Invalid operation.') from None
 
-    def _contact_mail(self):
+    def contact_mail(self):
         """Sends contact form emails"""
         try:
             dictionary = loads(self.data.decode())
@@ -79,7 +84,7 @@ class PrivateHandler(CommonBasicHandler):
             else:
                 return OK(msg)
 
-    def _tenant2tenant(self, maxlen=2048):
+    def tenant2tenant(self, maxlen=2048):
         """Stores tenant info"""
         try:
             message = self.data.decode()
@@ -94,7 +99,7 @@ class PrivateHandler(CommonBasicHandler):
                 TenantMessage.add(self.terminal, message)
                 return OK(status=201)
 
-    def _damage_report(self):
+    def damage_report(self):
         """Stores damage reports"""
         try:
             dictionary = loads(self.data.decode())
@@ -140,39 +145,53 @@ class PublicHandler(CommonBasicHandler):
     def task(self):
         """Returns the respective task"""
         try:
-            task = self.query['task']
+            return self.query['task']
         except KeyError:
             raise self.logerr('No task specified.') from None
-        else:
-            if task is None:
-                raise self.logerr('Task must not be null.') from None
-            else:
-                return task
+
+    @property
+    def document(self):
+        """Returns the appropriate statistics document"""
+        try:
+            return self.query['document']
+        except KeyError:
+            raise self.logerr('Document must not be null.') from None
+
+    @property
+    def pin(self):
+        """Returns the cleaning PIN"""
+        try:
+            return int(self.query['pin'])
+        except KeyError:
+            raise self.logerr('No PIN provided.') from None
+        except (TypeError, ValueError):
+            raise self.logerr('PIN must be an integer.') from None
 
     def get(self):
         """Handles GET requests"""
         if self.resource == 'command':
-            return self._list_commands()
+            return self.list_commands(self.customer, self.vid)
         elif self.resource == 'cleaning':
-            return self._list_cleanings()
+            return self.list_cleanings(self.terminal)
+        elif self.resource == 'proxy':
+            return self.proxy()
         else:
             raise self.logerr('Invalid operation.') from None
 
     def post(self):
         """Handles POST requests"""
         if self.resource == 'command':
-            return self._complete_command()
+            return self.complete_command(self.customer, self.vid, self.task)
         elif self.resource == 'statistics':
-            return self._add_statistics()
+            return self.add_statistics(
+                self.customer, self.vid, self.tid, self.document)
         elif self.resource == 'cleaning':
-            return self._add_cleaning()
+            return self.add_cleaning()
         else:
             raise self.logerr('Invalid operation.') from None
 
-    def _list_commands(self):
+    def list_commands(self, customer, vid):
         """Lists commands for the respective terminal"""
-        customer = self.customer
-        vid = self.vid
         tasks = []
 
         for command in Command.select().where(
@@ -183,20 +202,17 @@ class PublicHandler(CommonBasicHandler):
 
         return JSON(tasks)
 
-    def _list_cleanings(self):
+    def list_cleanings(self, terminal):
         """Lists cleaning entries for the respective terminal"""
         try:
-            address = self.terminal.location.address
+            address = terminal.location.address
         except AttributeError:
             raise self.logerr('Terminal has no address.') from None
         else:
             return JSON(CleaningDate.of(address, limit=10))
 
-    def _complete_command(self):
+    def complete_command(self, customer, vid, task):
         """Completes the provided command"""
-        customer = self.customer
-        vid = self.vid
-        task = self.task
         result = False
 
         for command in Command.select().where(
@@ -209,28 +225,8 @@ class PublicHandler(CommonBasicHandler):
 
         return OK('1' if result else '0')
 
-    def _add_statistics(self):
+    def add_statistics(self, customer, vid, tid, document):
         """Adds a new statistics entry"""
-        customer = self.customer
-        vid = self.vid
-
-        try:
-            tid = int(self.query['tid'])
-        except KeyError:
-            tid = None
-        except ValueError:
-            raise self.logerr('TID must be an integer.') from None
-        except TypeError:
-            tid = None
-
-        try:
-            document = self.query['document']
-        except KeyError:
-            raise self.logerr('Document must not be null.') from None
-        else:
-            if document is None:
-                raise self.logerr('Document must not be null.') from None
-
         try:
             Statistics.add(customer, vid, tid, document)
         except Exception as e:
@@ -238,28 +234,19 @@ class PublicHandler(CommonBasicHandler):
         else:
             return OK(status=201)
 
-    def _add_cleaning(self):
+    def add_cleaning(self, terminal, pin):
         """Adds a cleaning entry"""
         try:
-            pin = int(self.query['pin'])
-        except KeyError:
-            raise self.logerr('No PIN provided.') from None
-        except (TypeError, ValueError):
-            raise self.logerr('PIN must be an integer.') from None
+            user = CleaningUser.get(
+                (CleaningUser.pin == pin) &
+                (CleaningUser.customer == terminal.customer))
+        except DoesNotExist:
+            raise self.logerr('Invalid PIN.', status=403) from None
         else:
-            terminal = self.terminal
-
             try:
-                user = CleaningUser.get(
-                    (CleaningUser.pin == pin) &
-                    (CleaningUser.customer == terminal.customer))
-            except DoesNotExist:
-                raise self.logerr('Invalid PIN.', status=403) from None
+                address = terminal.location.address
+            except AttributeError:
+                raise self.logerr('Terminal has no address.') from None
             else:
-                try:
-                    address = terminal.location.address
-                except AttributeError:
-                    raise self.logerr('Terminal has no address.') from None
-                else:
-                    CleaningDate.add(user, address)
-                    return OK(status=201)
+                CleaningDate.add(user, address)
+                return OK(status=201)
