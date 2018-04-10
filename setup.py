cbe0c6cd0f5ebf4c3f64129363b2a38ed9db83f9
Richard Neumann <r.neumann@homeinfo.de>
2018-04-10 16:23:52 +0200

    Removed obsolete files.

--- setup.py
+++ setup.py
@@ -9,9 +9,4 @@ setup(
     author='Richard Neumann',
     requires=['homeinfo.crm', 'openimmo'],
     packages=['appcmd'],
-    scripts=['files/appcmd-private', 'files/appcmd-public'],
-    data_files=[
-        ('/usr/lib/systemd/system', [
-            'files/appcmd-private.service',
-            'files/appcmd-public.service'])],
     description='Digital Sigange Flash Application communication interface')
