02ff454e1cb3e60d4616527b682d2d89440ef79e
Richard Neumann <r.neumann@homeinfo.de>
2019-02-05 12:18:19 +0100

    Refac.

--- appcmd/lpt.py
+++ appcmd/lpt.py
@@ -1,10 +1,6 @@
 """Local public transportation API."""
 
-from datetime import datetime, timedelta
-
-from flask import request
-from lptlib import dom, get_departures as _get_departures, Stop, StopEvent
-from wsgilib import ACCEPT, JSON, XML
+from lptlib import get_response
 
 from appcmd.functions import get_terminal
 
@@ -12,32 +8,7 @@ from appcmd.functions import get_terminal
 __all__ = ['get_departures']
 
 
-def _get_departures_test():
-    """Returns a test set of departures."""
-
-    now = datetime.now()
-    on_time = StopEvent('18', now, None, 'Istanbul', 'S-Bahn')
-    on_time_explicit = StopEvent('Pünklicher Bus', now, now, 'Katwijk', 'Bus')
-    delay = timedelta(minutes=42)
-    delayed = StopEvent('Verspätetet', now, now + delay, 'Bonn', 'Fliewatüüt')
-    early = StopEvent('Überpünklich', now, now - delay, 'Berlin', 'Pferd')
-    departures = (on_time, on_time_explicit, delayed, early)
-    stop = Stop('Nächstgelegene Haltestelle', 'Nächstgelegene Haltestelle',
-                -38.2626511, 144.5796318, departures)
-    return [stop]
-
-
 def get_departures():
-    """Returns the respective departures."""
+    """Returns stops for the respective terminal."""
 
-    if 'test' in request.args:
-        stops = _get_departures_test()
-    else:
-        stops = _get_departures(get_terminal().address)
-
-    if 'application/json' in ACCEPT:
-        return JSON([stop.to_json() for stop in stops])
-
-    xml = dom.stops()
-    xml.stop = [stop.to_dom() for stop in stops]
-    return XML(xml)
+    return get_response(get_terminal().address)
