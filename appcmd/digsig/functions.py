f3daff5095aa271bac0b79608b86a3e05d52cc02
Richard Neumann <r.neumann@homeinfo.de>
2019-02-25 16:13:41 +0100

    Refac.

--- appcmd/digsig/functions.py
+++ appcmd/digsig/functions.py
@@ -20,7 +20,7 @@ def make_attachment(bytes_):
     return (FileMetaData.from_bytes(bytes_).filename, bytes_)
 
 
-def tar_file(tarfile, filename, bytes_):
+def _tar_file(tarfile, filename, bytes_):
     """Adds the respective bytes to the tar file."""
 
     tarinfo = TarInfo(filename)
@@ -41,10 +41,10 @@ def get_tar_stream(files, *, chunk_size=4096):
                 manifest.append(filename)
 
                 if sha256(bytes_).hexdigest() not in sha256sums:
-                    tar_file(tar, filename, bytes_)
+                    _tar_file(tar, filename, bytes_)
 
             manifest = dumps(manifest).encode()
-            tar_file(tar, 'manifest.json', manifest)
+            _tar_file(tar, 'manifest.json', manifest)
 
         tmp.flush()
         tmp.seek(0)
