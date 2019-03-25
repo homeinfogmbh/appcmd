52d3ceff40dfca5543c0b9139e131b0367ac29b4
Richard Neumann <r.neumann@homeinfo.de>
2019-03-25 16:24:36 +0100

    Fix.

--- appcmd/lpt.py
+++ appcmd/lpt.py
@@ -11,4 +11,6 @@ __all__ = ['get_departures']
 def get_departures():
     """Returns stops for the respective terminal."""
 
-    return get_response(get_terminal().address)
+    terminal = get_terminal()
+    address = terminal.lpt_address or terminal.address
+    return get_response()
