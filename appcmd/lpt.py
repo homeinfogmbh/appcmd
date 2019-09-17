12723d913b245e9220dcb09a3db49a5ac7c8ae0f
Richard Neumann <r.neumann@homeinfo.de>
2019-09-17 12:37:24 +0200

    Deactivated all parsing validation in appcmd/lpt.py.

--- appcmd/lpt.py
+++ appcmd/lpt.py
@@ -1,11 +1,18 @@
 """Local public transportation API."""
 
+from pyxb import RequireValidWhenParsing
+
 from lptlib import get_response
 
 from appcmd.functions import get_system
 
 
 __all__ = ['get_departures']
+
+
+# XXX: Deactivate all PyXB parsing validation
+# because of broken TRIAS APIS from VVS and EFA-BW.
+RequireValidWhenParsing(False)
 
 
 def get_departures():
