52a3a024ecc2de93a0322b372060a09c8a33097b
Richard Neumann <r.neumann@homeinfo.de>
2019-09-17 11:47:34 +0200

    Fixed renting.

--- appcmd/renting.py
+++ appcmd/renting.py
@@ -2,52 +2,84 @@
 
 from flask import request
 
+from rentallib import dom
 from rentallib import AlreadyRented
 from rentallib import EndBeforeStart
 from rentallib import DurationTooLong
 from rentallib import DurationTooShort
 from rentallib import Rentable
+from rentallib import Renting
 from timelib import strpdatetime
 from wsgilib import Error, OK
 
+from appcmd.functions import get_customer_and_address
 
-def rent():
+
+__all__ = ['list_rentables', 'list_rentings', 'submit_rent']
+
+
+def list_rentables():
+    """Lists available rentables."""
+
+    customer, _ = get_customer_and_address()
+    xml = dom.rentables()
+
+    for rentable in Rentable.select().where(Rentable.customer == customer):
+        xml.rentable.append(rentable.to_dom())
+
+    return xml
+
+
+def list_rentings():
+    """Lists stored rentings."""
+
+    customer, _ = get_customer_and_address()
+    xml = dom.rentings()
+
+    for renting in Renting.select().join(Rentable).where(
+            Rentable.customer == customer):
+        xml.renting.append(renting.to_dom())
+
+    return xml
+
+
+def submit_rent():
     """Rents a rentable."""
 
     try:
         rentable = request.json['rentable']
     except KeyError:
-        raise Error('No rentable specified.')
+        return Error('No rentable specified.')
 
     try:
         start = strpdatetime(request.json['start'])
     except KeyError:
-        raise Error('No start datetime specified.')
+        return Error('No start datetime specified.')
     except ValueError:
-        raise Error('Datetime must be in ISO format.')
+        return Error('Datetime must be in ISO format.')
 
     try:
         end = strpdatetime(request.json['end'])
     except KeyError:
-        raise Error('No end datetime specified.')
+        return Error('No end datetime specified.')
     except ValueError:
-        raise Error('Datetime must be in ISO format.')
+        return Error('Datetime must be in ISO format.')
 
     rentee = request.json.get('rentee')
 
     if not rentee:
-        raise Error('No rentee specified.')
+        return Error('No rentee specified.')
 
     customer = request.json.get('customer')
 
     if not customer:
-        raise Error('No customer specified.')
+        return Error('No customer specified.')
 
     try:
         rentable = Rentable.get(
             (Rentable.id == rentable) & (Rentable.customer == customer))
     except Rentable.DoesNotExist:
-        raise Error('No such rentable.', status=404)
+        return Error('No such rentable.', status=404)
 
     try:
         rentable.rent(rentee, start, end)
