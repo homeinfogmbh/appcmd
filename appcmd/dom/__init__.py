fd5bb5d132791cda9fc1c65a8a70f7aef4544922
Richard Neumann <r.neumann@homeinfo.de>
2019-11-26 17:24:48 +0100

    Removed commands dom module.

--- appcmd/dom/__init__.py
+++ appcmd/dom/__init__.py
@@ -1,6 +1,6 @@
 """Auto generated XSD DOMs for flash application."""
 
-from appcmd.dom import commands, garbage_collection
+from appcmd.dom import garbage_collection
 
 
-__all__ = ['commands', 'garbage_collection']
+__all__ = ['garbage_collection']
