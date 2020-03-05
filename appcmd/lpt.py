8dab816651c095fce25fb664fd25f1dc1791e729
Richard Neumann <r.neumann@homeinfo.de>
2020-03-05 15:45:25 +0100

    Refactored for less LOC.

--- appcmd/lpt.py
+++ appcmd/lpt.py
@@ -11,11 +11,9 @@ __all__ = ['get_departures']
 def get_departures():
     """Returns stops for the respective system."""
 
-    system = get_system()
-    deployment = system.deployment
+    deployment = get_system().deployment
 
     if deployment is None:
         return ('System is not deployed.', 400)
 
-    address = deployment.lpt_address or deployment.address
-    return get_response(address)
+    return get_response(deployment.lpt_address or deployment.address)
