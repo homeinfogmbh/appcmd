f8f409b3855ff65f1c0197c704094fb605cb9b11
Richard Neumann <mail@richard-neumann.de>
2021-01-22 12:08:25 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
 #! /usr/bin/env python3
 """Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='appcmd',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo priod de>',
-    requires=['mdb', 'openimmo', 'damage_report', 'tenant2tenant'],
+    install_requires=[
+        'aha',
+        'bookings',
+        'cleaninglog',
+        'cmslib',
+        'configlib',
+        'damage_report',
+        'digsigdb',
+        'emaillib',
+        'flask',
+        'hwdb',
+        'lptlib',
+        'mdb',
+        'peeweeplus',
+        'requests',
+        'setuptools',
+        'tenant2landlord',
+        'tenant2tenant',
+        'timelib',
+        'wsgilib',
+    ],
     packages=['appcmd', 'appcmd.dom', 'appcmd.update'],
     description='Digital Signange Flash Application communication interface.'
 )
