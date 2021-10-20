8315078da327439bffefd123b6f442af46d680d8
Richard Neumann <mail@richard-neumann.de>
2021-10-20 10:25:52 +0200

    Handle NoGeoCoordinatesForAddress

--- appcmd/lpt.py
+++ appcmd/lpt.py
@@ -2,7 +2,8 @@
 
 from flask import Response
 
-from lptlib import get_response
+from lptlib import NoGeoCoordinatesForAddress, get_response
+from wsgilib import JSONMessage
 
 from appcmd.functions import get_lpt_address
 
@@ -13,4 +14,8 @@ __all__ = ['get_departures']
 def get_departures() -> Response:
     """Returns stops for the respective system."""
 
-    return get_response(get_lpt_address())
+    try:
+        return get_response(get_lpt_address())
+    except NoGeoCoordinatesForAddress as error:
+        return JSONMessage('No geo coordinates for address.',
+                           address=error.address, status=404)
