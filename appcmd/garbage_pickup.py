8df8d9e3c402556cf9fb6a65dccecc4e2b55dd0d
Richard Neumann <r.neumann@homeinfo.de>
2021-12-08 20:25:54 +0100

    Add per-day caching

--- appcmd/garbage_pickup.py
+++ appcmd/garbage_pickup.py
@@ -1,5 +1,6 @@
 """Garbage collection info retrieval."""
 
+from datetime import date
 from typing import Union
 
 from aha import AmbiguousLocations
@@ -8,12 +9,34 @@ from aha import NoLocationFound
 from aha import ScrapingError
 from aha import find_location
 from aha import get_pickups
+from aha import Location
+from aha import Pickup
 from wsgilib import JSON, JSONMessage
 
 from appcmd.functions import get_address
 
 
 __all__ = ['garbage_pickup']
+
+
+CACHE = {}
+
+
+def get_cached_pickups(location: Location, house_number: str) -> list[Pickup]:
+    """Returns cached pickups for the day."""
+
+    try:
+        locations = CACHE[date.today()]
+    except KeyError:
+        CACHE.clear()
+        CACHE[date.today()] = locations = {}
+
+    try:
+        return locations[location]
+    except KeyError:
+        locations[location] = pickups = list(
+            get_pickups(location, house_number))
+        return pickups
 
 
 def garbage_pickup() -> Union[JSON, JSONMessage]:
@@ -30,7 +53,7 @@ def garbage_pickup() -> Union[JSON, JSONMessage]:
                            locations=[l.name for l in locations])
 
     try:
-        pickups = get_pickups(location, address.house_number)
+        pickups = get_cached_pickups(location, address.house_number)
     except HTTPError as error:
         return JSONMessage('HTTP error.', status_code=error.status_code)
     except ScrapingError as error:
