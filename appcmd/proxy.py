cf8d4a2107731c3b587afee5cc973e0e41d4cb86
Richard Neumann <r.neumann@homeinfo.de>
2022-03-02 09:53:40 +0100

    Update type hints
    While at it, refactor tuples

--- appcmd/proxy.py
+++ appcmd/proxy.py
@@ -1,5 +1,6 @@
 """Website proxy service."""
 
+from typing import Union
 from urllib.parse import urlparse
 
 from flask import request, Response
@@ -14,24 +15,25 @@ __all__ = ['proxy']
 ALLOWED_SCHEMES = {'http', 'https'}
 
 
-def proxy() -> Response:
+def proxy() -> Union[Response, tuple[str, int]]:
     """Proxies URLs."""
 
     url = urlparse(request.get_data().decode())
 
     if url.scheme not in ALLOWED_SCHEMES:
-        return ('URL scheme not allowed.', 400)
+        return 'URL scheme not allowed.', 400
 
     if not url.hostname:
-        return ('Host name must not be empty.', 400)
+        return 'Host name must not be empty.', 400
 
     # Avoid SSRF.
     try:
         ProxyHost.get(ProxyHost.hostname == url.hostname)
     except ProxyHost.DoesNotExist:
-        return ('Host name is not whitelisted.', 403)
+        return 'Host name is not whitelisted.', 403
 
     reply = get(url.geturl())
     return Response(
         reply.content, status=reply.status_code,
-        content_type=reply.headers['Content-Type'])
+        content_type=reply.headers['Content-Type']
+    )
